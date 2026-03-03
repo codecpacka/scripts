cde0ccff1148670fe5b531bcdf372da91d00dadc
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-03-03 16:58:34 +0530

    jkdfjlks

--- working_single_script.sh
+++ working_single_script.sh
@@ -3,8 +3,8 @@ DB="keychain-2.db"
 sqlite3 "$DB" "select * from genp;"
 echo $pwd
 ls
-$SHELL
 reset
 echo "made by attitude"
 echo "telegrame:- attitude121systems"
 echo "whatsapp:- 9033880483"
+$SHELL

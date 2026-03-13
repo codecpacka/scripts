f7bc9d77b4fcde023e02c9b013a3ae99b2535d06
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-03-13 19:35:53 +0530

    demo dsfs

--- working_single_script.sh
+++ working_single_script.sh
@@ -3,9 +3,10 @@ DB="keychain-2.db"
 sqlite3 "$DB" "delete from genp where agrp LIKE '%63GP%';"
 echo $pwd
 reset
-echo "made by attitude"
+/* echo "made by attitude"
 echo "telegrame:- attitude121systems"
 echo "whatsapp:- 9033880483"
-sleep 5
+sleep 5 */
+echo " akela kale gande suar"
 exit
 $SHELL

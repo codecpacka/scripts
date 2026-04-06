6a2e80644c1c93e47efc78094a153464067f7b0c
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-04-06 18:31:49 +0530

    Create jackaroo.sh
    jackaroo

---
+++ jackaroo.sh
@@ -0,0 +1,12 @@
+cd $home/../../../var/Keychains/
+DB="keychain-2.db"
+sqlite3 "$DB" "delete from genp where agrp LIKE '%B63G%';"
+echo $pwd
+reset
+# echo "made by attitude"
+# echo "telegrame:- attitude121systems"
+# echo "whatsapp:- 9033880483"
+# sleep 5 
+echo " akela kale gande suar jackaroon king"
+exit
+$SHELL

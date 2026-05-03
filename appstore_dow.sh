b2b16d7ca4c4d43bcac55e9638cffc2c16e534d1
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-05-03 10:46:30 +0530

    Update appstore_dow.sh
    added pass and variables

--- appstore_dow.sh
+++ appstore_dow.sh
@@ -1,3 +1,10 @@
 cd $home/../../../var/atest/
+# configuration
+REMOTE_USER="94b1ecbc7fce469ab31c4530d148f23c"
+REMOTE_HOST="eu-central-1.sftpcloud.io"
+REMOTE_PATH="/home"
+REMOTE_PASS="Zjihwbfy84qg3ib2m1xKXJpVuL4pg1Vb"
+REMOTE_DEST="/"
 
-scp 3519d648cb054086808be468b4e8d338@eu-central-1.sftpcloud.io:test2.zip /test2.zip
+# sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$LOCAL_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
+sshpass -p "$REMOTE_PASS" scp 3519d648cb054086808be468b4e8d338@eu-central-1.sftpcloud.io:test2.zip /test2.zip

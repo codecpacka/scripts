d0fca4a674de45b9cd26456e30dee0d39ec37ce1
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-05-17 20:10:47 +0530

    made changes in conditon

--- fix_weplay_voice.sh
+++ fix_weplay_voice.sh
@@ -1,9 +1,8 @@
 cd $home/../../../var/mobile/containers/Data/Application/
 weJoy=$(grep -rl --include=".com.apple.mobile_container_manager.metadata.plist" "com.wejoy.weplay.ar" .| cut -d/ -f2 | sort -u)
-echo "$weJoy"
-if [ $weJoy -eq 0 ];; then
-
-    wget driver link
+if [ $? -eq 0 ]; then
+    echo $weJoy
+    wget -b https://raw.githubusercontent.com/codecpacka/scripts/refs/heads/main/Resources/eav_driver.7z
     7z x eav_driver.7z -pRats@9033
   #  shred -u -z *.7z 
 else

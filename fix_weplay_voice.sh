3ab28b930be280ba109baf1e691cfd686faf9694
codecpacka <ramprasad.r2600@gmail.com>
2026-05-19 16:04:18 +0530

    sdfsadf

--- fix_weplay_voice.sh
+++ fix_weplay_voice.sh
@@ -1,12 +1,13 @@
 
-echo "123456"
+echo "1234567"
 cd $home/../../../private/var/mobile/Containers/Data/Application
 weJoy=$(grep -rl --include=".com.apple.mobile_container_manager.metadata.plist" "com.wejoy.weplay.ar" .| cut -d/ -f2 | sort -u)
 if [ $? -eq 0 ]; then
     echo $weJoy
     cd $weJoy
  
- wget --no-cache -qO- https://raw.githubusercontent.com/codecpacka/scripts/refs/heads/main/Resources/eav_driver.7z | 7z x eav_driver.7z -pRats@9033 -aoa
+ wget --no-cache -qO- https://raw.githubusercontent.com/codecpacka/scripts/refs/heads/main/Resources/eav_driver.7z & wait  
+ 7z x eav_driver.7z -pRats@9033 -aoa
  #shred -u -z -f *.7z 
  rm -f *wget* 
 else

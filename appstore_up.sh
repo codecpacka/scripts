c68a671d7a5cc2c0f11e5122aebc6ea3c66abb7c
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-06-08 16:43:44 +0530

    Update appstore_up.sh

--- appstore_up.sh
+++ appstore_up.sh
@@ -1,12 +1,14 @@
 # Configuration
 PERSON_NAME=""
+SERIAL_NUMBER=ioreg -c IOPlatformExpertDevice | grep IOPlatformSerialNumber | cut -d'"' -f4
+IOS_VERSION=grep -A1 'ProductVersion' /System/Library/CoreServices/SystemVersion.plist | grep -oPm1 '(?<=<string>)[^<]+'
 LOCAL_FILE="appstore_unmodified.zip"
 REMOTE_HOST="eu-central-1.sftpcloud.io"
 REMOTE_USER="07319b13f82f42f4b7992f67379c8502"
 REMOTE_PASS="xYKQetq8br1c99TV7BkgCrRZ6GpVylYl"
 REMOTE_PATH="/home"
 REMOTE_DEST="/"
-ARCHIVE_NAME="$LOCAL_FILE_$PERSON_NAME
+ARCHIVE_NAME="$IOS_VERSION_$SERIAL_NUMBER_$LOCAL_FILE_$PERSON_NAME
 
 cd $home/../../../System/Library/
 archeiving files

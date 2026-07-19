c947397203ca32a6b71d3d631a41fd132c4082ed
codecpacka <32800467+codecpacka@users.noreply.github.com>
2026-07-19 16:40:32 +0530

    Update icD.sh

--- icD.sh
+++ icD.sh
@@ -1,4 +1,4 @@
-echo "<=======  icd: --2222=======>"
+echo "<=======  icd: -44443=======>"
 #
 # <============server configuration==============>
 #
@@ -17,7 +17,7 @@ ARCHIVE_NAME="${IOS_VERSION}_${SERIAL_NUMBER}_${LOCAL_FILE}_${PERSON_NAME}.zip"
 echo "$ARCHIVE_NAME"
 
 
-cd $home/../../../Containers/Data/Application
+cd $home/../../../containers/Bundle/Application
 echo $pwd
 mega=$(grep -rl --include=".iTunesMetadata.plist" "com.smudgeapps.megaphonefree" .| cut -d/ -f2 | sort -u)
 echo $mega
